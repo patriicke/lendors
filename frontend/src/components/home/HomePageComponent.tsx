c284e6f59591b3fd1e92892b7b88477ccfd51774
patriicke <patrickndayambaje4@gmail.com>
2022-11-04 15:03:27 +0200

    [updated]: I have made teh home page responsive

--- frontend/src/components/home/HomePageComponent.tsx
+++ frontend/src/components/home/HomePageComponent.tsx
@@ -4,12 +4,13 @@ import React, { useState } from "react";
 import Bg_2 from "/assets/bg/bg_02.jpg";
 import Icon_1 from "/icons/icon_01.png";
 import Img_1 from "/assets/img/img_01.jpg";
+import { Slide } from "react-awesome-reveal";
 const HomePageComponent: React.FC = () => {
   const [amount, setAmount] = useState<number>(1500000);
   const categories: string[] = ["All", " Sedan", "Sports", "Luxury"];
   const [currentCategory, setCurrentCategory] = useState<number>(0);
   return (
-    <div className="h-[calc(100vh_-_5rem)] min-h-[calc(100vh_-_5rem)] w-full relative">
+    <div className="h-[calc(100vh_-_5rem)] min-h-[calc(100vh_-_5rem)] w-full relative overflow-auto">
       <div className="h-[70rem] max-h-[80%] w-full absolute top-0 -z-50">
         <img
           src={Bg_2}
@@ -19,35 +20,24 @@ const HomePageComponent: React.FC = () => {
         />
       </div>
       <div className="w-full h-[75%] flex items-center  justify-center gap-10 z-20 flex-col bg-slate-800 bg-opacity-40">
-      
-        <div className="flex flex-col items-center w-full gap-8">
-          <h1 className="font-bold text-5xl w-[30%] text-center capitalize text-white">
-            {"Lamborghini Aventador LP700-4".toUpperCase()}
-          </h1>
-          <p className="text-white text-xl">
-            6.6L V8 32V DDI OHV Turbo Diesel, 5-Speed Automatic, Fuel Type:
-            Diesel, Color: Black
-          </p>
-          <button className="bg-redish text-white flex items-center justify-center gap-3 px-14 py-4 mt-7 rounded-md font-bold text-xl hover:bg-red-800 duration-300">
-            <span>BOOK NOW</span>
-            <img src={Icon_1} alt="arrow" />
-          </button>
-        </div>
-        <div className="flex items-center justify-center gap-5 mt-[12rem]">
-          {Array(3)
-            .fill("")
-            .map((text, index) => (
-              <button
-                className={`w-24 h-4 bg-redish ${
-                  index == 0 ? "bg-opacity-80" : "bg-opacity-40"
-                }`}
-                key={index}
-              ></button>
-            ))}
-        </div>
+        <Slide direction="up">
+          <div className="flex flex-col items-center w-full gap-8 text-center">
+            <h1 className="font-bold text-4xl  xl:text-5xl w-[50%] text-center capitalize text-white">
+              {"Lamborghini Aventador LP700-4".toUpperCase()}
+            </h1>
+            <p className="text-white text-xl">
+              6.6L V8 32V DDI OHV Turbo Diesel, 5-Speed Automatic, Fuel Type:
+              Diesel, Color: Black
+            </p>
+            <button className="bg-redish text-white flex items-center justify-center gap-3 px-14 py-4 mt-7 rounded-md font-bold text-xl hover:bg-red-800 duration-300">
+              <span>BOOK NOW</span>
+              <img src={Icon_1} alt="arrow" />
+            </button>
+          </div>
+        </Slide>
       </div>
-      <section className="h-40 w-4/5 bg-blueish m-auto z-40 px-5 flex items-center gap-8 justify-center rounded-md shadow-md">
-        <div className="flex flex-col gap-2 w-[15em]">
+      <section className="min-h-[5em] w-4/5 bg-blueish m-auto z-40 px-5 flex items-center gap-8 justify-center rounded-md shadow-md flex-wrap overflow-auto py-5">
+        <div className="flex flex-col gap-2 w-[16em]">
           <h1 className="text-white text-lg font-bold">Pick Up Location</h1>
           <div className="h-14 p-2 bg-white rounded-md flex justify-between items-center">
             <input
@@ -61,7 +51,7 @@ const HomePageComponent: React.FC = () => {
             />
           </div>
         </div>
-        <div className="flex flex-col gap-2 w-[15em]">
+        <div className="flex flex-col gap-2 w-[16em]">
           <h1 className="text-white text-lg font-bold">Pick Up Date</h1>
           <div className="h-14 p-2 bg-white rounded-md flex justify-between items-center">
             <input
@@ -90,7 +80,7 @@ const HomePageComponent: React.FC = () => {
             <h1>45,000,000 RWF</h1>
           </div>
         </div>
-        <button className="bg-redish text-white flex items-center justify-center gap-3 px-14 py-4 mt-7 rounded-md font-bold text-xl hover:bg-red-800 duration-300">
+        <button className="bg-redish text-white flex items-center justify-center gap-3 w-[13em] h-14 mt-7 rounded-md font-bold text-xl hover:bg-red-800 duration-300">
           <span>FIND A CAR</span>
           <img src={Icon_1} alt="arrow" />
         </button>
