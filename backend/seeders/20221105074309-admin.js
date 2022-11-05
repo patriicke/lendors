3141273bcb77b1fa1e79c0ef671a7bd61867aa42
patriicke <patrickndayambaje4@gmail.com>
2022-11-05 13:08:27 +0200

    [swagger]: This is json file for the swagger documentation

--- backend/seeders/20221105074309-admin.js
+++ backend/seeders/20221105074309-admin.js
@@ -1,19 +1,21 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Users', [{
-      names: "Ndayambaje Patrick",
-      email: 'patrickndayambaje@gmail.com',
-      role: 'admin',
-      address: "Kigali, Rwanda",
-      telephone: "+250782307144",
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }]);
+    return queryInterface.bulkInsert("Users", [
+      {
+        names: "Patrick NDAYAMBAJE",
+        email: "patrickndayambaje@gmail.com",
+        role: "admin",
+        address: "Kigali, Rwanda",
+        telephone: "+250790603658",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ]);
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('Users', null, {});
+    return queryInterface.bulkDelete("Users", null, {});
   }
 };
