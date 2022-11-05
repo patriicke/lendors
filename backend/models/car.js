7c5127e13d869409cb5a6a638fa0326ff8ac6ae1
patriicke <patrickndayambaje4@gmail.com>
2022-11-05 12:13:36 +0200

    [feature]: These are the required models

---
+++ backend/models/car.js
@@ -0,0 +1,31 @@
+"use strict";
+const { v4 } = require("uuid");
+const DataTypes = require("sequelize/lib/data-types");
+const { sequelize } = require("./../utils/database");
+const { registerSchema } = require("swaggiffy");
+
+const Car = sequelize.define(
+  "Car",
+  {
+    id: { type: DataTypes.UUID, allowNull: false, primaryKey: true },
+    name: { type: DataTypes.STRING, allowNull: false },
+    brand: { type: DataTypes.STRING, allowNull: false },
+    description: { type: DataTypes.STRING, allowNull: false },
+    imageUrl: { type: DataTypes.STRING, allowNull: false },
+    added: { type: DataTypes.INTEGER, allowNull: false },
+    price: { type: DataTypes.INTEGER, allowNull: false },
+    isBooked: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+      allowNull: false
+    },
+    currency: { type: DataTypes.STRING, allowNull: false }
+  },
+  {}
+);
+
+//registerSchema('Car', sequelize.createSchema('Car'), { orm: 'sequelize' });
+
+module.exports = () => {
+  return Car;
+};
---
+++ backend/models/user.js
@@ -0,0 +1,35 @@
+"use strict";
+const { v4 } = require("uuid");
+const DataTypes = require("sequelize/lib/data-types");
+const { sequelize } = require("../utils/database");
+const { registerSchema } = require("swaggiffy");
+
+const User = sequelize.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: v4(),
+      primaryKey: true
+    },
+    names: { type: DataTypes.STRING, allowNull: false },
+    email: { type: DataTypes.STRING, allowNull: false },
+    address: { type: DataTypes.STRING, allowNull: false },
+    joined: {
+      type: DataTypes.INTEGER,
+      defautValue: Date.now(),
+      allowNull: false
+    },
+    telephone: { type: DataTypes.STRING, allowNull: false },
+    role: { type: DataTypes.STRING, defautValue: "user", allowNull: false },
+    password: { type: DataTypes.STRING, allowNull: false }
+  },
+  {}
+);
+
+//registerSchema('User', sequelize.createSchema('User'), { orm: 'sequelize' });
+
+module.exports = () => {
+  return User;
+};
