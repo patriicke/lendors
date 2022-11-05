0ae66122b7d74ac6aa8f42c8fecce89ea7ed9a84
patriicke <patrickndayambaje4@gmail.com>
2022-11-05 11:45:12 +0200

    [feature]: This is the utilities directory for the cors, database and email utilities

---
+++ backend/utils/email.js
@@ -0,0 +1,45 @@
+const dotenv = require("dotenv");
+const nodemailer = require("nodemailer");
+dotenv.config();
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  pool: true,
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASSWORD
+  }
+});
+
+transporter.verify(function (error, success) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Server is ready to take our messages");
+  }
+});
+
+exports.mailTo = async (email, text, html, subject) => {
+  try {
+    const info = await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to: email,
+      subject,
+      text,
+      html
+    });
+    console.log(info.messageId);
+    console.log("Testing email functionality");
+    return {
+      message: "Email sent successfully",
+      emailId: info.messageId,
+      status: true
+    };
+  } catch (error) {
+    console.log(error);
+    return { message: "Unable to send email", status: false };
+  }
+};
