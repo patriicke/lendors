a3e19fa2eddb74055b26cc7d59311c491976efda
patriicke <patrickndayambaje4@gmail.com>
2022-11-05 12:06:28 +0200

    [feature]: This is the user controller

--- backend/controllers/request.controller.js
+++ backend/controllers/request.controller.js
@@ -1,44 +1,54 @@
-const User = require('./../models/user')
-const Car = require('./../models/car')
-const Request = require('./../models/request')
-const { mailTo } = require('../utils/email')
-const { v4 } = require('uuid')
-
+const User = require("./../models/user");
+const Car = require("./../models/car");
+const Request = require("./../models/request");
+const { mailTo } = require("../utils/email");
+const { v4 } = require("uuid");
 
 exports.newRequest = async (req, res) => {
-    try {
-        const userId = req.user.userId
-        console.log(userId);
-        const id = `${v4()}-${Math.floor(Math.random() * 9999)}`
-        const { carId, startDate, endDate } = req.body
-        const car = await Car().findOne({ where: { id: carId } })
-        if (!car) return res.status(400).json({ message: "Car does not exist" })
-        if (car.isBooked) return res.status(400).json({ message: "Car is already booked" })
-        const request = await Request().create({
-            carId, id, startDate, endDate,
-            userId
-        })
-        console.log(car);
-        await car.update({ ...car, isBooked: true })
-        return res.status(200).json({ message: "Request created successfully", request })
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({ message: "Internal server error", error: error.message })
-    }
-}
+  try {
+    const userId = req.user.userId;
+    console.log(userId);
+    const id = `${v4()}-${Math.floor(Math.random() * 9999)}`;
+    const { carId, startDate, endDate } = req.body;
+    const car = await Car().findOne({ where: { id: carId } });
+    if (!car) return res.status(400).json({ message: "Car does not exist" });
+    if (car.isBooked)
+      return res.status(400).json({ message: "Car is already booked" });
+    const request = await Request().create({
+      carId,
+      id,
+      startDate,
+      endDate,
+      userId
+    });
+    console.log(car);
+    await car.update({ ...car, isBooked: true });
+    return res
+      .status(200)
+      .json({ message: "Request created successfully", request });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
+};
 
 exports.grantRequest = async (req, res) => {
-    try {
-        const { requestId } = req.params
-        const request = await Request().findOne({ where: { id: requestId } })
-        if (!request) return res.status(400).json({ message: "Request not found" })
-        const car = await Car().findOne({ where: { id: request.carId } })
-        console.log(car);
-        const user = await User().findOne({ where: { id: request.userId } })
-        if (!user) return res.status(400).json({ messsage: "User does not exist" })
-        console.log(user);
-        await request.update({ status: 'granted' })
-        await mailTo(user.email, '', `    
+  try {
+    const { requestId } = req.params;
+    const request = await Request().findOne({ where: { id: requestId } });
+    if (!request) return res.status(400).json({ message: "Request not found" });
+    const car = await Car().findOne({ where: { id: request.carId } });
+    console.log(car);
+    const user = await User().findOne({ where: { id: request.userId } });
+    if (!user) return res.status(400).json({ messsage: "User does not exist" });
+    console.log(user);
+    await request.update({ status: "granted" });
+    await mailTo(
+      user.email,
+      "",
+      `    
 <style>
   @import url('https://fonts.googleapis.com/css2?family=Fugaz+One&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700&display=swap');
 .div{
@@ -93,26 +103,36 @@ font-size:x-large;
   </div>
            <a href="http://lendrvercel.app/request/id" class="button">VIEW REQUEST</a>
 </div>
-        `, 'Car Request Grant ')
+        `,
+      "Car Request Grant "
+    );
 
-        return res.status(200).json({ message: "Request granted successfully", car, request })
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({ message: "Internal server error", error: error.message })
-    }
-}
+    return res
+      .status(200)
+      .json({ message: "Request granted successfully", car, request });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
+};
 exports.denyRequest = async (req, res) => {
-    try {
-        const { requestId } = req.params
-        const request = await Request().findOne({ where: { id: requestId } })
-        if (!request) return res.status(400).json({ message: "Request not found" })
-        const car = await Car().findOne({ where: { id: request.carId } })
+  try {
+    const { requestId } = req.params;
+    const request = await Request().findOne({ where: { id: requestId } });
+    if (!request) return res.status(400).json({ message: "Request not found" });
+    const car = await Car().findOne({ where: { id: request.carId } });
 
-        await car.update({ isBooked: false })
-        await request.update({ status: 'denied' })
-        return res.status(200).json({ message: "Request denied successfully", car, request })
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({ message: "Internal server error", error: error.message })
-    }
-}
+    await car.update({ isBooked: false });
+    await request.update({ status: "denied" });
+    return res
+      .status(200)
+      .json({ message: "Request denied successfully", car, request });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
+};
---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,175 @@
+const User = require("../models/user");
+const Car = require("../models/car");
+const { mailTo } = require("../utils/email");
+const dateFns = require("date-fns");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
+
+exports.createAccount = async (req, res) => {
+  try {
+    const { names, email, address, telephone, password } = req.body;
+    const joined = Date.now();
+    const hashedPassword = await bcrypt.hash(password, 8);
+    const user = await User().create({
+      names,
+      email,
+      role: "user",
+      joined,
+      address,
+      telephone,
+      password: hashedPassword
+    });
+    console.log(user);
+    return res
+      .status(200)
+      .json({ message: "Account created successfully", user });
+  } catch (error) {
+    console.log(error.errors[0].message);
+    return res.status(500).json({ message: error.errors[0].message });
+  }
+};
+
+exports.allUsers = async (req, res) => {
+  try {
+    console.log("Accessing all user route");
+    const users = await User().findAll();
+    if (!users)
+      return res.status(200).json({ message: "Users not found", users });
+    return res
+      .status(200)
+      .json({ message: "Users fetched successfully", users });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Internal server error",
+      error: error.message
+    });
+  }
+};
+
+exports.getUser = async (req, res) => {
+  try {
+    const { userId } = req.user;
+    const user = await User().findOne({ where: { id: userId } });
+    if (!user)
+      return res.status(500).json({ message: "Error getting user data" });
+    return res.status(200).json({ message: "Getting usser successfull", user });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await (await User().findOne({ email })).get({ plain: true });
+    if (!user)
+      return res.status(400).json({ message: "Email entered does not exist" });
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ message: "Wrong password" });
+    const isAdmin = user.role === "admin" ? true : false;
+    const token = await jwt.sign(
+      { userId: user.id, isAdmin },
+      process.env.JWT_SECRET_KEY,
+      {}
+    );
+    const returnedUser = { ...user };
+    delete returnedUser.password;
+    return res
+      .status(200)
+      .json({ message: "Logged in successfully", user: returnedUser, token });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
+};
+
+exports.carRequest = async (req, res) => {
+  try {
+    const { carId } = req.params;
+    const { userId } = req.user;
+    const { startDate, endDate } = req.body;
+
+    const user = await User().findOne({ where: { id: userId } });
+
+    const car = await Car().findOne({ where: { id: carId } });
+    if (!car) return res.status(400).json({ message: "Car not found" });
+
+    const request = Request.create({
+      startDate,
+      endDate,
+      userId,
+      carId
+    });
+
+    if (!request)
+      return res.status(500).json({ message: "Unable to create car request" });
+
+    await mailTo(
+      user.email,
+      "Hello there",
+      `<b>Car ${car.name} is being requested by ${user.names}(you)</b>.\n 
+            <p>You needs to use it from <strong>${dateFns.format(
+              startDate,
+              "MMM do YYYY"
+            )} until ${(endDate, "MMM do YYYY")}.</strong>\n</p>
+            <br><br>
+            `,
+      `Car Request by ${user.names} via drive`
+    );
+    return res
+      .status(200)
+      .json({ message: "Car request submitted successfully", car });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
+};
+
+exports.searchUsers = async (req, res) => {
+  try {
+    const { query } = req.params;
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
+};
+
+exports.deleteUser = async (req, res) => {
+  try {
+    const { userId } = req.body;
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
+};
+
+exports.updateUser = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const { name, email, address, telephone } = req.body;
+
+    const user = User().findOne({ where: { id: userId } });
+    await user.update({ name, email, address, telephone });
+    if (!user) return res.status(400).json({ message: "User doen't exist" });
+    return res.status(200).json({ message: "User updated succesfully" });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
+};
