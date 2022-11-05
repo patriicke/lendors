1342ae07fa4d2cb4a83c344ce5c505eb118dbbae
patriicke <patrickndayambaje4@gmail.com>
2022-11-05 19:20:27 +0200

    [update]: The swaggify file

---
+++ backend/config/config.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+module.exports = {
+  development: {
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+    dialect: "postgres"
+  },
+  test: {
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+    dialect: "postgres"
+  },
+  production: {
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+    dialect: "postgres"
+  }
+};
