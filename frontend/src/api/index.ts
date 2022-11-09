fd5b6ee4bd58e73ab31d0a5b75e15ac4989f8e22
patriicke <patrickndayambaje4@gmail.com>
2022-11-10 00:10:12 +0200

    [fix]: Reconnected backend

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BACKEND_URL: string = "http://localhost:5000";
+const BACKEND_URL: string = "https://lendors-api.herokuapp.com";
 
 const api = axios.create({
   baseURL: BACKEND_URL
