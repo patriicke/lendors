07db095c0204d84f9a2e33e543e902fc7c771832
patriicke <patrickndayambaje4@gmail.com>
2022-11-24 18:42:56 +0200

    [update]: I have added most powerful way to view the car and request it

--- frontend/src/components/home/HomePageComponent.tsx
+++ frontend/src/components/home/HomePageComponent.tsx
@@ -22,32 +22,32 @@ const HomePageComponent: React.FC = () => {
     document.title = "Lendors";
   }, []);
   return (
-    <div className="h-[calc(100vh_-_5rem)] min-h-[calc(100vh_-_5rem)] w-full relative">
-      <div className="h-[70rem] max-h-[80%] w-full absolute top-0 -z-50">
+    <div className='h-[calc(100vh_-_5rem)] min-h-[calc(100vh_-_5rem)] w-full relative'>
+      <div className='h-[70rem] max-h-[80%] w-full absolute top-0 -z-50'>
         <img
           src={Bg_2}
-          alt="Lamborghini"
-          className="h-full w-full object-cover"
+          alt='Lamborghini'
+          className='h-full w-full object-cover'
           draggable={false}
         />
       </div>
-      <div className="w-full h-[75%] flex items-center  justify-center gap-10 z-20 flex-col bg-slate-800 bg-opacity-40">
-        <Slide direction="up" cascade triggerOnce>
-          <div className="flex flex-col items-center w-full gap-8 text-center">
-            <h1 className="font-bold text-4xl  xl:text-5xl w-[50%] text-center capitalize text-white">
+      <div className='w-full h-[75%] flex items-center  justify-center gap-10 z-20 flex-col bg-slate-800 bg-opacity-40'>
+        <Slide direction='up' cascade triggerOnce>
+          <div className='flex flex-col items-center w-full gap-8 text-center'>
+            <h1 className='font-bold text-4xl  xl:text-5xl w-[50%] text-center capitalize text-white'>
               {"Lamborghini Aventador LP700-4".toUpperCase()}
             </h1>
             <Fade
               cascade
               duration={20}
               triggerOnce
-              className="text-white text-xl"
+              className='text-white text-xl'
             >
               6.6L V8 32V DDI OHV Turbo Diesel, 5-Speed Automatic, Fuel Type:
               Diesel, Color: Black
             </Fade>
             <button
-              className="bg-redish text-white flex items-center justify-center gap-3 px-14 py-4 mt-7 rounded-md font-bold text-xl hover:bg-red-800 duration-300"
+              className='bg-redish text-white flex items-center justify-center gap-3 px-14 py-4 mt-7 rounded-md font-bold text-xl hover:bg-red-800 duration-300'
               onClick={() => {
                 !userSlice?.isLoggedIn
                   ? setLoginPage(true)
@@ -55,43 +55,43 @@ const HomePageComponent: React.FC = () => {
               }}
             >
               <span>BOOK NOW</span>
-              <img src={Icon_1} alt="arrow" />
+              <img src={Icon_1} alt='arrow' />
             </button>
           </div>
         </Slide>
       </div>
-      <Slide direction="up" cascade triggerOnce>
-        <section className="min-h-[5em] w-4/5 bg-blueish m-auto z-40 px-5 flex items-center gap-8 justify-center rounded-md shadow-md flex-wrap overflow-auto py-5">
-          <div className="flex flex-col gap-2 w-[16em]">
-            <h1 className="text-white text-lg font-bold">Pick Up Location</h1>
-            <div className="h-14 p-2 bg-white rounded-md flex justify-between items-center">
+      <Slide direction='up' cascade triggerOnce>
+        <section className='min-h-[5em] w-4/5 bg-blueish m-auto z-40 px-5 flex items-center gap-8 justify-center rounded-md shadow-md flex-wrap overflow-auto py-5'>
+          <div className='flex flex-col gap-2 w-[16em]'>
+            <h1 className='text-white text-lg font-bold'>Pick Up Location</h1>
+            <div className='h-14 p-2 bg-white rounded-md flex justify-between items-center'>
               <input
-                type="text"
-                placeholder="City, State or Airport Code"
-                className="h-full px-3 outline-none"
+                type='text'
+                placeholder='City, State or Airport Code'
+                className='h-full px-3 outline-none'
               />
               <FontAwesomeIcon
                 icon={faLocationDot}
-                className="text-black text-lg"
+                className='text-black text-lg'
               />
             </div>
           </div>
-          <div className="flex flex-col gap-2 w-[16em]">
-            <h1 className="text-white text-lg font-bold">Pick Up Date</h1>
-            <div className="h-14 p-2 bg-white rounded-md flex justify-between items-center">
+          <div className='flex flex-col gap-2 w-[16em]'>
+            <h1 className='text-white text-lg font-bold'>Pick Up Date</h1>
+            <div className='h-14 p-2 bg-white rounded-md flex justify-between items-center'>
               <input
-                type="date"
-                placeholder="City, State or Airport Code"
-                className="h-full px-3 outline-none w-full"
+                type='date'
+                placeholder='City, State or Airport Code'
+                className='h-full px-3 outline-none w-full'
               />
             </div>
           </div>
-          <div className="flex flex-col gap-2 w-[16em] justify-between">
-            <h1 className="text-white text-lg font-bold">Minimun Price</h1>
+          <div className='flex flex-col gap-2 w-[16em] justify-between'>
+            <h1 className='text-white text-lg font-bold'>Minimun Price</h1>
             <input
-              type="range"
-              placeholder="City, State or Airport Code"
-              className="h-full outline-none bg-gray-200 w-full"
+              type='range'
+              placeholder='City, State or Airport Code'
+              className='h-full outline-none bg-gray-200 w-full'
               min={800000}
               max={20000000}
               step={100000}
@@ -100,37 +100,37 @@ const HomePageComponent: React.FC = () => {
                 setAmount(Number(e.target.value));
               }}
             />
-            <div className="text-white flex justify-between">
+            <div className='text-white flex justify-between'>
               <h1>{new Intl.NumberFormat("en-us").format(amount)} RWF</h1> -
               <h1>20,000,000 RWF</h1>
             </div>
           </div>
           <button
-            className="bg-redish text-white flex items-center justify-center gap-3 w-[13em] h-14 mt-7 rounded-md font-bold text-xl hover:bg-red-800 duration-300"
+            className='bg-redish text-white flex items-center justify-center gap-3 w-[13em] h-14 mt-7 rounded-md font-bold text-xl hover:bg-red-800 duration-300'
             onClick={() => navigate("/gallery")}
           >
             <span>FIND A CAR</span>
-            <img src={Icon_1} alt="arrow" />
+            <img src={Icon_1} alt='arrow' />
           </button>
         </section>
       </Slide>
-      <section className="h-[3em] w-full"></section>
-      <Slide direction="up" cascade triggerOnce>
-        <section className="w-full h-20 min-h-20 flex flex-col items-center  gap-5">
-          <div className="w-full flex items-center  justify-center gap-3 ">
-            <div className="h-2 w-16 bg-redish"></div>
-            <h1 className="font-bold text-4xl text-center">
+      <section className='h-[3em] w-full'></section>
+      <Slide direction='up' cascade triggerOnce>
+        <section className='w-full h-20 min-h-20 flex flex-col items-center  gap-5'>
+          <div className='w-full flex items-center  justify-center gap-3 '>
+            <div className='h-2 w-16 bg-redish'></div>
+            <h1 className='font-bold text-4xl text-center'>
               Featured Vehicles
             </h1>
-            <div className="h-2 w-16 bg-redish"></div>
+            <div className='h-2 w-16 bg-redish'></div>
           </div>
-          <p className="text-xl p-1 text-center">
+          <p className='text-xl p-1 text-center'>
             These are one of the best vehicles. Be the first to go with.
           </p>
         </section>
       </Slide>
-      <section className="h-[50em] py-8">
-        <div className="w-full xl:w-[70%] m-auto py-4 flex flex-wrap gap-6 items-center justify-center  transition-all duration-300">
+      <section className='h-[50em] py-8'>
+        <div className='w-full xl:w-[70%] m-auto py-4 flex flex-wrap gap-6 items-center justify-center  transition-all duration-300'>
           {(user.role == "admin" ? [...allCars] : [...cars])
             ?.sort((a: any, b: any) => {
               return (
@@ -141,36 +141,36 @@ const HomePageComponent: React.FC = () => {
             .splice(0, 6)
             .map((car: CarObject, index: number) => {
               return (
-                <Slide direction="up" key={index} triggerOnce>
-                  <div className="w-96  h-[513px] rounded relative overflow-hidden shadow-lg cursor-pointer">
+                <Slide direction='up' key={index} triggerOnce>
+                  <div
+                    className='w-96  h-[520px] rounded relative overflow-hidden shadow-lg cursor-pointer'
+                    onClick={() => navigate(`/car/${car.id}`)}
+                  >
                     <img
-                      className="w-full h-64"
+                      className='w-full h-64'
                       src={car.imageUrl}
-                      alt="Sunset in the mountains"
+                      alt='Sunset in the mountains'
                     />
-                    <div className="px-6 py-4">
-                      <div className="font-bold text-xl mb-2">{car.name}</div>
-                      <p className="text-gray-700 text-base">
+                    <div className='px-6 py-4'>
+                      <div className='font-bold text-xl mb-2'>{car.name}</div>
+                      <p className='text-gray-700 text-base'>
                         {car.description}
                       </p>
                     </div>
-                    <div className="inline-flex -space-x-px absolute bottom-6 px-2 gap-3">
-                      <button className="p-3 ml-0 leading-tight text-gray-500 bg-white rounded-l-lg border border-gray-200 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white duration-75 flex gap-1">
+                    <div className='inline-flex -space-x-px absolute bottom-6 px-2 gap-3'>
+                      <button className='p-2 ml-0 leading-tight text-gray-500 bg-white rounded-l-lg border border-gray-200 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white duration-75 flex gap-1'>
                         {new Intl.NumberFormat("es-us").format(
                           Number(car.price)
                         )}
-                        <span className="font-bold">
+                        <span className='font-bold'>
                           {car.currency.toUpperCase()}
                         </span>
                       </button>
-                      <button
-                        className="p-3 leading-tight text-white bg-redish rounded-r-lg border border-gray-200 hover:bg-red-600 hover:text-white dark:bg-dispatch dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white duration-75"
-                        onClick={() => navigate(`/car/${car.id}`)}
-                      >
+                      <button className='p-2 leading-tight text-white bg-redish rounded-r-lg border border-gray-200 hover:bg-red-600 hover:text-white dark:bg-dispatch dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white duration-75'>
                         View Details
                       </button>
                       {user.role == "admin" && car.isBooked && (
-                        <button className="p-3 leading-tight text-white bg-blue-500 rounded-r-lg border border-gray-200 hover:bg-blue-600 hover:text-white dark:bg-dispatch dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white duration-75">
+                        <button className='p-2 leading-tight text-white bg-blue-500 rounded-r-lg border border-gray-200 hover:bg-blue-600 hover:text-white dark:bg-dispatch dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white duration-75'>
                           BOOKED
                         </button>
                       )}
--- frontend/src/pages/Gallery/GalleryPage.tsx
+++ frontend/src/pages/Gallery/GalleryPage.tsx
@@ -19,50 +19,51 @@ const GalleryPage: React.FC = () => {
   }, []);
   return (
     <div className={`h-[calc(100vh_-_5rem)] w-full mt-5`}>
-      <Slide direction="up" cascade triggerOnce>
-        <section className="w-full h-20 min-h-20 flex flex-col items-center  gap-5">
-          <div className="w-full flex items-center  justify-center gap-3 ">
-            <div className="h-2 w-16 bg-redish"></div>
-            <h1 className="font-bold text-4xl text-center">
+      <Slide direction='up' cascade triggerOnce>
+        <section className='w-full h-20 min-h-20 flex flex-col items-center  gap-5'>
+          <div className='w-full flex items-center  justify-center gap-3 '>
+            <div className='h-2 w-16 bg-redish'></div>
+            <h1 className='font-bold text-4xl text-center'>
               Featured Vehicles
             </h1>
-            <div className="h-2 w-16 bg-redish"></div>
+            <div className='h-2 w-16 bg-redish'></div>
           </div>
-          <p className="text-xl p-1 text-center">
+          <p className='text-xl p-1 text-center'>
             These are one of the best vehicles. Be the first to go with.
           </p>
         </section>
       </Slide>
-      <section className="h-[50em] py-8">
-        <div className="w-full xl:w-[70%] m-auto py-4 flex flex-wrap gap-6 items-center justify-center  transition-all duration-300">
+      <section className='h-[50em] py-8'>
+        <div className='w-full xl:w-[70%] m-auto py-4 flex flex-wrap gap-6 items-center justify-center  transition-all duration-300'>
           {(user.role == "admin" ? [...allCars] : [...cars])?.map(
             (car: CarObject, index: number) => {
               return (
-                <div className="w-96  h-[513px] rounded relative overflow-hidden shadow-lg cursor-pointer" key={index}>
+                <div
+                  className='w-96  h-[513px] rounded relative overflow-hidden shadow-lg cursor-pointer'
+                  key={index}
+                  onClick={() => navigate(`/car/${car.id}`)}
+                >
                   <img
-                    className="w-full h-64"
+                    className='w-full h-64'
                     src={car.imageUrl}
-                    alt="Sunset in the mountains"
+                    alt='Sunset in the mountains'
                   />
-                  <div className="px-6 py-4">
-                    <div className="font-bold text-xl mb-2">{car.name}</div>
-                    <p className="text-gray-700 text-base">{car.description}</p>
+                  <div className='px-6 py-4'>
+                    <div className='font-bold text-xl mb-2'>{car.name}</div>
+                    <p className='text-gray-700 text-base'>{car.description}</p>
                   </div>
-                  <div className="inline-flex -space-x-px absolute bottom-6 px-2 gap-3">
-                    <button className="p-3 ml-0 leading-tight text-gray-500 bg-white rounded-l-lg border border-gray-200 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white duration-75 flex gap-1">
+                  <div className='inline-flex -space-x-px absolute bottom-6 px-2 gap-3'>
+                    <button className='p-2 ml-0 leading-tight text-gray-500 bg-white rounded-l-lg border border-gray-200 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white duration-75 flex gap-1'>
                       {new Intl.NumberFormat("es-us").format(Number(car.price))}
-                      <span className="font-bold">
+                      <span className='font-bold'>
                         {car.currency.toUpperCase()}
                       </span>
                     </button>
-                    <button
-                      className="p-3 leading-tight text-white bg-redish rounded-r-lg border border-gray-200 hover:bg-red-600 hover:text-white dark:bg-dispatch dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white duration-75"
-                      onClick={() => navigate(`/car/${car.id}`)}
-                    >
+                    <button className='p-2 leading-tight text-white bg-redish rounded-r-lg border border-gray-200 hover:bg-red-600 hover:text-white dark:bg-dispatch dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white duration-75'>
                       View Details
                     </button>
                     {user.role == "admin" && car.isBooked && (
-                      <button className="p-3 leading-tight text-white bg-blue-500 rounded-r-lg border border-gray-200 hover:bg-blue-600 hover:text-white dark:bg-dispatch dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white duration-75">
+                      <button className='p-2 leading-tight text-white bg-blue-500 rounded-r-lg border border-gray-200 hover:bg-blue-600 hover:text-white dark:bg-dispatch dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white duration-75'>
                         BOOKED
                       </button>
                     )}
@@ -72,10 +73,10 @@ const GalleryPage: React.FC = () => {
             }
           )}
         </div>
-        <div className="w-full py-5 flex items-center justify-center ">
-          <div className="inline-flex -space-x-px">
+        <div className='w-full py-5 flex items-center justify-center '>
+          <div className='inline-flex -space-x-px'>
             <button
-              className="p-3 ml-0 leading-tight text-gray-500 bg-white rounded-l-lg border border-gray-200 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white duration-75"
+              className='p-3 ml-0 leading-tight text-gray-500 bg-white rounded-l-lg border border-gray-200 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white duration-75'
               onClick={() => {
                 currentCars == 1
                   ? setCurrentCars(cars.length)
@@ -85,7 +86,7 @@ const GalleryPage: React.FC = () => {
               Previous
             </button>
             <button
-              className="p-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-200 hover:bg-red-600 hover:text-white dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white duration-75"
+              className='p-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-200 hover:bg-red-600 hover:text-white dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white duration-75'
               onClick={() => {
                 currentCars == cars.length
                   ? setCurrentCars(1)
